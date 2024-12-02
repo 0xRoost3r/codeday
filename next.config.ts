5914b0befaa651bc49cec25d8173710f440c1f61
Vo Thuong Nguyen <9839951+0xRoost3r@users.noreply.github.com>
2024-12-02 16:51:02 +0700

    WIP: Order api
    - Remove bot route
    + add prisma connector
    + supabase client

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
+import { NextConfig } from "next";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
-  },
-  serverExternalPackages:  ['grammy'],
+  }
 };
 
 export default nextConfig;
---
+++ src/app/api/order/route.ts
@@ -0,0 +1,49 @@
+export const revalidate = 60;
+import { createClient } from '@/utils/supabase/client'
+
+// Define the interface for transaction data
+interface Transaction {
+    confirmed: boolean;
+    chainId: string;
+    txs: Array<{
+        hash: string;
+        fromAddress: string;
+        toAddress: string;
+        value: string;
+    }>;
+}
+
+export async function POST(request: Request) {
+  try {
+    const supabase = createClient()
+    const body = await request.json();
+
+    // Ensure payload matches expected structure
+    const { confirmed, txs, chainId }: Transaction = body;
+
+    if (!txs || txs.length === 0) {
+      return Response.json({ error: "No transactions found" }, { status: 400 });
+    }
+
+    // Extract data from the first transaction
+    const { fromAddress, toAddress, value, hash } = txs[0];
+
+    const { error } = await supabase.from('order').upsert({
+        hash: hash,
+        fromAddress: fromAddress,
+        value: value,
+        chainId: chainId,
+        confirmed: confirmed,
+      })
+      if (error) throw error
+
+    return Response.json({
+      confirmed,
+      fromAddress,
+      toAddress,
+      value,
+    });
+  } catch (error) {
+    return Response.json({ error: "Invalid request body" }, { status: 400 });
+  }
+}
