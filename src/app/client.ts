7b9d6a1feec0a6df3d23801cb89881d32c921abf
Vo Thuong Nguyen <9839951+0xRoost3r@users.noreply.github.com>
2024-12-04 21:46:29 +0700

    Fix logic buy button

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = "c89163254bf7c464852e9f8b5d4a12fb";
+const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
 
 if (!clientId) {
   throw new Error("No client ID provided");
